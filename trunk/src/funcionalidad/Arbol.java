87828f12243db4aada30dbacd3084d2c4bc5822b
santaigomoral <santaigomoral@58827990-c5f4-2de2-2bf7-fa68655f4825>
2011-06-04 21:43:17 +0000

    Agregados archivo que informa que tiene que hacer cada uno

--- trunk/src/funcionalidad/Arbol.java
+++ trunk/src/funcionalidad/Arbol.java
@@ -4,10 +4,10 @@ import java.util.Vector;
 
 public class Arbol extends Resultado{
 	private Matriz matriz;
+	private Columna columnaMayorGanancia;
 	private Vector<Resultado> resultados;
-	private String nombreColumnaMayorGanancia;
 		
-	public String obtenerCategoria(Matriz matrizDeDatos) {
+	public String obtenerCategoria(Matriz matrizDeDatos) { 
 		if (resultados == null){
 			procesarResultados();
 		}
@@ -15,6 +15,8 @@ public class Arbol extends Resultado{
 		/*sabiendo cual es la columna con mayor ganacia, se busca en la matriz de datos el valor 
 		 *que esta columna tiene y a partir de este valor se busca cual es su categoria en el vector 
 		 *resultados (encontrando el Resultado con ese valor y aplicando el metodo obtenerCategoria a ese Resultado*/
+		
+		
 		return null;
 	}
 
